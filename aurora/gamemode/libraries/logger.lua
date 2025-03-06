c378f03d8517ecda792238dfa1e0f892674f9ccc
Florian <mrminecrafttv19@gmail.com>
2025-03-06 21:21:11 +0100

    docs: add luadoc commentary

--- aurora/gamemode/libraries/logger.lua
+++ aurora/gamemode/libraries/logger.lua
@@ -1,10 +1,17 @@
 aurora = aurora or {}
 aurora.log = {}
 
+--- Gets the current timestamp in a formatted string
+-- @realm shared
+-- @return string Formatted timestamp [YYYY-MM-DD HH:MM:SS]
 local function getTimestamp()
     return os.date("[%Y-%m-%d %H:%M:%S]")
 end
 
+--- Internal function to handle log message formatting and display
+-- @realm shared
+-- @param color table The color for the message text
+-- @param ... any Additional arguments to be printed
 local function localLog(color, ...)
     local args = {}
 
@@ -19,18 +26,31 @@ local function localLog(color, ...)
     MsgC(Color(0, 255, 255), "", args[1], args[2], color, " ", ...)
 end
 
+--- Logs a debug message to the console
+-- @realm shared
+-- @param ... any Values to log (will be concatenated with spaces)
 function aurora.log.debug(...)
     localLog(Color(184, 158, 42), "[DEBUG] ", ..., "\n")
 end
 
+--- Logs an info message to the console
+-- @realm shared
+-- @param ... any Values to log (will be concatenated with spaces)
 function aurora.log.info(...)
     localLog(Color(0, 255, 255), "[INFO] ", Color(255,255,255), ..., "\n")
 end
 
+--- Logs a warning message to the console
+-- @realm shared
+-- @param ... any Values to log (will be concatenated with spaces)
 function aurora.log.warn(...)
     localLog(Color(255, 255, 0), "[WARN] ", ..., "\n")
 end
 
+--- Logs an error message to the console and saves it to a file
+-- @realm shared
+-- @param ... any Values to log (will be concatenated with spaces)
+-- @note This function also saves the error message to aurora/error_logs.txt
 function aurora.log.error(...)
     localLog(Color(255, 0, 0), "[ERROR] ", ..., "\n")
 
