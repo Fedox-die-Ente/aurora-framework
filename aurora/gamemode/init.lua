b6c0a44f3a3565077647fdf917d8402577fe0056
Florian <mrminecrafttv19@gmail.com>
2025-03-06 04:43:24 +0100

    feat: include shared file, and print a banner to the console after a change/server-start

--- aurora/gamemode/init.lua
+++ aurora/gamemode/init.lua
@@ -1,6 +1,37 @@
 GM.Name = "Aurora"
-GM.Author = "Fedox"
-GM.Version = "1.0"
+GM.Author = "Aurora Studios"
+GM.Version = "1.0.0"
+
+AddCSLuaFile("shared.lua")
+include("shared.lua")
 
 DeriveGamemode("sandbox")
 DEFINE_BASECLASS("gamemode_sandbox")
+
+--- Displays the gamemode banner in the console with version and author information
+-- @realm shared
+-- @param[opt] color table The color to display the banner in (defaults to color_white)
+-- @return boolean True if banner was displayed successfully, false otherwise
+function aurora.printBanner(color)
+    local bannerFilePath = "gamemodes/aurora/banner.txt"
+    local displayColor = color or color_white
+
+    if not file.Exists(bannerFilePath, "GAME") then
+        ErrorNoHaltWithStack(string.format("Could not find banner file at %s", bannerFilePath))
+        return false
+    end
+
+    local bannerContent = file.Read(bannerFilePath, "GAME")
+    if not bannerContent then
+        ErrorNoHaltWithStack(string.format("Could not read banner file at %s", bannerFilePath))
+        return false
+    end
+
+    bannerContent = bannerContent:gsub("{version}", GM.Version or "Unknown")
+                                :gsub("{author}", GM.Author or "Unknown")
+
+    MsgC(displayColor, bannerContent)
+    return true
+end
+
+aurora.printBanner()
