f41ec6b96b17f2e612816da80d5cd74cf1aab6c5
Bozy <smartypants245@gmail.com>
2025-03-06 14:45:12 -0500

    feat: add logger, 4 logging levels
    Includes 4 Logging levels

---
+++ aurora/gamemode/libraries/logger.lua
@@ -0,0 +1,32 @@
+aurora = aurora or {}
+aurora.log = {}
+
+local function localLog(color, ...)
+    local args = {}
+
+    if CLIENT then
+        args[1] = Color(227, 216, 115)
+        args[2] = "[CLIENT] "
+    else
+        args[1] = Color(0, 150, 255)
+        args[2] = "[SERVER] "
+    end
+
+    MsgC(Color(0, 255, 255), "[Aurora] ", args[1], args[2], color, ...)
+end
+
+function aurora.log.Debug(...)
+    localLog(Color(184, 158, 42), "[DEBUG] ", ..., "\n")
+end
+
+function aurora.log.Info(...)
+    localLog(Color(0, 255, 255), "[INFO] ", Color(255,255,255), ..., "\n")
+end
+
+function aurora.log.Warn(...)
+    localLog(Color(255, 255, 0), "[WARN] ", ..., "\n")
+end
+
+function aurora.log.Error(...)
+    localLog(Color(255, 0, 0), "[ERROR] ", ..., "\n")
+end
