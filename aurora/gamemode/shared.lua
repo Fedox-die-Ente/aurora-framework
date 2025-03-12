b0abf021ed4f1ae93495e2e61ce8f26395b6f638
Florian <mrminecrafttv19@gmail.com>
2025-03-12 05:46:46 +0100

    feat(shared): use new refresh function

--- aurora/gamemode/shared.lua
+++ aurora/gamemode/shared.lua
@@ -3,4 +3,17 @@
 -- @realm shared
 aurora = aurora or {}
 
-aurora.util.includeDir("libraries")
+-- Uh, i have this because gmod is weird sometimes.
+LOADED = false
+
+function GM:Initialize()
+    aurora.util.refresh()
+end
+
+function GM:OnReloaded()
+    if not LOADED then
+        -- aurora.util.includeDir("libraries")
+        aurora.util.refresh()
+        LOADED = true
+    end
+end 
